ca98dedc478609ca4a3fe04e2f865bdaf3ff9bcb
Terry Chou <terrychoucn@gmail.com>
2020-01-09 14:59:45 +0800

    Initial Commit

---
+++ ivish/ivish.h
@@ -0,0 +1,19 @@
+//
+//  ivish.h
+//  ivish
+//
+//  Created by Terry Chou on 1/9/20.
+//  Copyright © 2020 Boogaloo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ivish.
+FOUNDATION_EXPORT double ivishVersionNumber;
+
+//! Project version string for ivish.
+FOUNDATION_EXPORT const unsigned char ivishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ivish/PublicHeader.h>
+
+
